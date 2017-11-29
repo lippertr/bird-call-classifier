229376b7f158442da23641e9938895b0e7aca04b
Ralph Lippert <ralph.lippert@rdlippert.com>
2017-11-29 00:13:21 -0700

    updated readme

--- README.md
+++ README.md
@@ -4,18 +4,35 @@
 A project to classify birds by their songs and calls
 
 ## Table of Contents
-TODO as contents entered
 
 [Schedule](#schedule)
 
+
+## Overview Pipeline
+Data in cloud
+Gather html data for bird species
+Scrape for species pages
+Access species pages and scrape for mp3 urls
+Download mp3 urls into master audio store
+Process audio with selected features to output a sonogram
+Process sonogram with CNN to classify bird song or calls
+
+## Data
+- 40 classes of bird species.
+- 33,567 seperate audio files.
+- 85G of audio data.
+- Used 70/30% split for 40 classes and 22K samples in train set.
+- 13K sample validation set.
+
+
 ## Data Sources
-http://www.xeno-canto.org/
-https://www.mbr-pwrc.usgs.gov/id/calllist.html
-https://nationalzoo.si.edu/scbi/migratorybirds/education/nasongkey.pl
-https://www.bird-sounds.net/
-http://www.birds.cornell.edu/Page.aspx%3Fpid%3D1059
-http://ebird.org/content/ebird/
-https://secure.birds.cornell.edu/cassso/login?service=https%3A%2F%2Fmacaulaylibrary.org%2Fauth%2Fclo%2Fcallback%3Furl%3Dhttps%253A%252F%252Fmacaulaylibrary.org%252Fsignin
+- http://www.xeno-canto.org/
+- https://www.mbr-pwrc.usgs.gov/id/calllist.html
+- https://nationalzoo.si.edu/scbi/migratorybirds/education/nasongkey.pl
+- https://www.bird-sounds.net/
+- http://www.birds.cornell.edu/Page.aspx%3Fpid%3D1059
+- http://ebird.org/content/ebird/
+- https://secure.birds.cornell.edu/cassso/login?service=https%3A%2F%2Fmacaulaylibrary.org%2Fauth%2Fclo%2Fcallback%3Furl%3Dhttps%253A%252F%252Fmacaulaylibrary.org%252Fsignin
 
 white paper:
 https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/43905.pdf
--- src/utils_cnn_birds.py
+++ src/utils_cnn_birds.py
@@ -101,6 +101,68 @@ class util_cnn_birds():
         #     f.close()
         return
 
+    def create_meta_db(self):
+        '''
+        just csv store for now but move to postgres as more data added
+        '''
+
+            f = open('raw_target_data_url.txt', 'r')
+            raw_data = f.read()
+            f.close()
+            data_rows = raw_data.split('\n')
+
+            #file_label, species_label, audio_id, url
+            #make a list of dictionaries with those keys and turn into a dataframe then csv it to file
+            df_list = []
+            counter = len(data_rows)
+
+            count = 0
+            while (count<counter-1):
+
+                row_dict = {}
+                start = data_rows[count].split(':')
+                file_label = start[0]
+                row_label = file_label
+                # print('counter:{} count:{}'.format(counter,count))
+                species_label = start[2].split('/')
+                species_label = species_label[-1].split("'")
+                species_label = species_label[0].lower()
+                # print(file_label)
+                while (file_label == row_label):
+                    count += 1
+                    # if(count==counter):
+                    #     sys.exit(0)
+                    #get the next row and process
+                    row = data_rows[count]
+                    labels = row.split(':')
+                    row_label = labels[0]
+
+                    #we need to be sure have a url row
+                    if(file_label == row_label):
+                        # print(row)
+                        audio_id = row.split("div id='")
+                        if(len(audio_id)<2):
+                            continue
+                        audio_id = audio_id[1].split("'")
+                        audio_id = audio_id[0]
+                        # print(audio_id)
+                        url = row.split("data-xc-filepath='")
+                        url = url[1].split("'")
+                        url = url[0]
+                        #print('dictionary\nfile_label:{}\nspecies_label:{}\n \
+                        #audio_label:{}\nurl:{}\n'.format(file_label, species_label, audio_id, url))
+                        data_dict = {}
+                        data_dict['file_label'] = file_label
+                        data_dict['species_label'] = species_label
+                        data_dict['audio_id'] = audio_id
+                        data_dict['url'] = url
+                        print(data_dict)
+                        df_list.append(data_dict)
+
+
+            df = pd.DataFrame.from_dict(df_list)
+            df.to_csv('song_bird_database.csv') #rename this so I don't trash each time
+
 
 if __name__ == '__main__':
     util = util_cnn_birds()
